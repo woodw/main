e5f3160ae27b8076793492453e35b70d42802e71
William Wood <william.wood@vineyardcolumbus.org>
2012-11-15 22:43:29 -0500

    Adding the stuff I've been working on so far
    adding design elements 1-4

---
+++ js/main.js
@@ -0,0 +1,9 @@
+require(['jquery-1.8.2.min','bootstrap.min.js','less-1.3.1.min'],function($ww){
+	$ww = jQuery.noConflict();
+	console.log($ww);
+	console.log('Just Checking THings Out From Here');
+
+	//http://www.ilovefreesoftware.com/02/articles/free-website-with-free-hosting-and-free-domain-name.html
+		
+	//	http://www.freewebmasterhelp.com/tutorials/phpmysql/2
+});
